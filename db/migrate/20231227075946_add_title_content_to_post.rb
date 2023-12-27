7bb7a0fc7a24155144e359d90cf40f5eeb91ce1c
zhaoxm <hizhaoxm@gmail.com>
2023-12-27 16:14:38 +0800

    add Posts

---
+++ db/migrate/20231227075946_add_title_content_to_post.rb
@@ -0,0 +1,6 @@
+class AddTitleContentToPost < ActiveRecord::Migration[7.1]
+  def change
+    add_column :posts, :title, :string
+    add_column :posts, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_21_135142) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_075946) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "content"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
